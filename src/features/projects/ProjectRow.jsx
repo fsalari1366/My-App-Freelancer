128c84ee4426b2116e32d73e51a94e7dde66847d
faride_salari <fsalarimanesh@gmail.com>
2024-06-13 19:19:40 +0330

    projects developed

---
+++ src/features/projects/ProjectRow.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Table from '../../ui/Table'
+import truncateText from '../../utiles/truncateText'
+import { toPersianNumbersWithComma } from '../../utiles/toPersianNumbers'
+import toLocalDateShort from '../../utiles/toLocalDateShort'
+import { HiOutlineTrash } from 'react-icons/hi'
+import { TbPencilMinus } from "react-icons/tb"
+import Modal from '../../ui/Modal'
+
+const ProjectRow = ({ project, index }) => {
+    const [isEditOpen, setIsEditOpen] = useState(false);
+  return (
+    <Table.Row key={project._id}>
+                <td>{index + 1}</td>
+                <td>{truncateText(project.title, 30)}</td>
+                <td>{project.category.title}</td>
+                <td>{toPersianNumbersWithComma(project.budget)}</td>
+                <td>{toLocalDateShort(project.deadline)}</td>
+                <td>
+                    <div className="flex flex-wrap items-center gap-2 max-w-[200px]">
+                    {project.tags.map((tag)=> (
+                    <span className="bage badge--secondary" key={tag}>{tag}</span>
+                ))}
+                    </div>
+                </td>
+                <td>{project.freelancer?.name || "-"}</td>
+                <td>{project.status === "OPEN" ? (
+                     <span className="badge badge--success">باز</span>
+                ) : (
+                    <span className="badge badge--danger">بسته</span>
+                )}</td>
+                <td>
+                <div className="flex items-center gap-x-4">
+                    <button onClick={() => setIsEditOpen(true)}>
+                    <TbPencilMinus className="w-5 h-5 text-primary-900" />
+                    </button>
+                    <Modal open={isEditOpen} title="modal title" onClose={() => setIsEditOpen(false)}>
+                        this is modal...
+                    </Modal>
+                    <button>
+                    <HiOutlineTrash className="w-5 h-5 text-error" />
+                    </button>
+                    </div>
+                </td>
+            </Table.Row>
+  )
+}
+
+export default ProjectRow
--- src/features/projects/ProjectTable.jsx
+++ src/features/projects/ProjectTable.jsx
@@ -1,20 +1,19 @@
 import useOwnerProjects from "./useOwnerProjects";
 import Loading from "../../ui/Loading";
 import Empty from "../../ui/Empty";
+import Table from "../../ui/Table";
+import ProjectRow from "./ProjectRow";
 
 function ProjectTable() {
   const { isLoading, projects } = useOwnerProjects();
 
   if (isLoading) return <Loading />;
 
-  // console.log(projects);
   if (projects.length) return <Empty resourceName="پروژه" />;
 
   return (
-    <div className="bg-secondary-0 overflow-x-auto">
-    <table>
-      <thead>
-        <tr className='title-row'>
+    <Table>
+      <Table.Header>
         <th>#</th>
         <th>عنوان پروژه</th>
         <th>دسته بندی</th>
@@ -24,36 +23,13 @@ function ProjectTable() {
         <th>فریلنسر</th>
         <th>وضعیت</th>
         <th>عملیات</th>
-        </tr>
-      </thead>
-      <tbody>
-        {projects.map((project, index) => (
-            <tr key={project._id}>
-                <td>{index + 1}</td>
-                <td>{project.title}</td>
-                <td>{project.category.title}</td>
-                <td>{project.budget}</td>
-                <td>{project.deadline}</td>
-                <td>
-                    <div className="flex flex-wrap items-center gap-2 max-w-[200px]">
-                    {project.tags.map((tag)=> (
-                    <span className="bage badge--secondary" key={tag}>{tag}</span>
-                ))}
-                    </div>
-                </td>
-                <td>{project.freelancer?.name || "-"}</td>
-                <td>{project.status === "OPEN" ? (
-                     <span className="badge badge--success">باز</span>
-                ) : (
-                    <span className="badge badge--danger">بسته</span>
-                )}</td>
-                <td>...</td>
-            </tr>
-        
+        </Table.Header>
+      <Table.Body>
+      {projects.map((project, index) => (
+            <ProjectRow key={project._id} project={project} index={index} />
         ))}
-      </tbody>
-    </table>
-    </div>
+      </Table.Body>
+      </Table>
   );
 }
 export default ProjectTable;
