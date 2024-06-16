4dbb59d2b4772132971f8aad10e85c9ce29bd48e
faride_salari <fsalarimanesh@gmail.com>
2024-06-16 12:50:48 +0330

    edit

--- src/features/projects/CreateProjectForm.jsx
+++ src/features/projects/CreateProjectForm.jsx
@@ -13,19 +13,20 @@ const onSubmit =(data) => {
 
   return (
     <form className="space-y-8" onSubmit={handleSubmit(onSubmit)}>
-        <TextField label="عنوان پروژه"
-          name="title"
-          register={register}
-          required
-          validationSchema={{
-            required: "عنوان ضروری است",
-            minLength: {
-              value: 10,
-              message: "طول عنوان نامعتبر است"
-            }
-          }}
-          errors={errors}
-         />
+        <TextField
+        label="عنوان"
+        name="title"
+        register={register}
+        required
+        validationSchema={{
+          required: "عنوان ضروری است",
+          minLength: {
+            value: 10,
+            message: "حداقل 10 کاراکتر را وارد کنید",
+          },
+        }}
+        errors={errors}
+      />
 
         <TextField label="توضیحات"
           name="description"
@@ -41,19 +42,17 @@ const onSubmit =(data) => {
           errors={errors}
          />
 
-         <TextField label="بودجه"
-          name="budget"
-          register={register}
-          required
-          validationSchema={{
-            required: "توضیحات ضروری است",
-            minLength: {
-              value: 10,
-              message: "طول توضیحات نامعتبر است"
-            }
-          }}
-          errors={errors}
-         />
+        <TextField
+        label="بودجه"
+        name="budget"
+        type="number"
+        register={register}
+        required
+        validationSchema={{
+          required: "بودجه ضروری است",
+        }}
+        errors={errors}
+      />
          <button type='submit' className='btn btn--primary w-full'>تایید</button>
     </form>
   )
